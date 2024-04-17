c21d7c3537725316ad48ed5b31662594c14582d7
salman-905 <king_luffy2012@hotmail.com>
2024-04-18 00:58:13 +0300

    homework3

---
+++ src/test/java/Homework_03.java
@@ -0,0 +1,44 @@
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Homework_03 {
+  /*
+        Given
+            https://reqres.in/api/users/3
+        When
+            User sends a GET Request to the url
+        Then
+            HTTP Status Code should be 200
+        And
+            Content Type should be JSON
+        And
+            Status Line should be HTTP/1.1 200 OK
+     */
+
+
+    @Test
+    public void testApi()
+    {
+
+        String url = " https://reqres.in/api/users/3";
+        Response response = given().get(url);
+        response.prettyPrint();
+
+        //  HTTP Status Code should be 200
+        response.then().statusCode(200);
+
+       // Content Type should be JSON
+
+        response.then().contentType("application/json");
+
+        //    Status Line should be HTTP/1.1 200 OK
+        response.then().statusLine("HTTP/1.1 200 OK");
+
+
+
+
+    }
+
+}
